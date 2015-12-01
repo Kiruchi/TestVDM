a5049f914bdedc3b122e1caa8f7bd96cf08d179e
Pierre MARTIN <pierre.martin49@gmail.com>
2015-12-01 22:56:19 +0100

    First scraping function nearly working

--- getPosts.js
+++ getPosts.js
@@ -1,20 +1,56 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-var pageNumber = 0;
-var url = 'http://www.viedemerde.fr/?page=' + pageNumber;
+var url = 'http://www.viedemerde.fr/?page=';
 var classPosts = '.article';
 
 var vdmPosts = [];
+var id = 0;
 
-request(url, function(error, response, html){
-    if (!error && response.statusCode == 200) {
-        var $ = cheerio.load(html);
-        var posts = $(classPosts);
-        $(posts).each(function (i, post) {
-            console.log($(post).text());
-        });
-    } else {
-        console.log("Oops, I can't access " + url);
-    }
-});
+// Scraping 10 first pages
+for (var nbPage = 0; nbPage < 10; nbPage++) {
+    var urlSpec = url + nbPage;
+
+    // Do a GET to the URL
+    request(urlSpec, function (error, response, html) {
+        if (!error && response.statusCode == 200) {
+            var $ = cheerio.load(html);
+            var posts = $(classPosts);
+            $(posts).each(function () {
+                // Increment id
+                id++;
+
+                // Scraping data with Cheerio
+                var curPost = $(this);
+
+                // Get post content
+                var contentPost = curPost.children('p').text();
+                // Get infos (date & author)
+                var infos = curPost.children('.date').children('.right_part').children('p').eq(1).text();
+                // Parse date to the right format
+                var mySplit = infos.split('Le ')[1].split(' à ');
+                var date = mySplit[0];
+                var heure = mySplit[1];
+                mySplit = heure.split(' - ');
+                heure = mySplit[0];
+                var pattern = /^(\d{1,2})\/(\d{1,2})\/(\d{4})$/;
+                var arrayDate = date.match(pattern);
+                var datePost = arrayDate[3] + "-" + arrayDate[2] + "-" + arrayDate[1] + " " + heure + ":00";
+                // Parse author
+                var author = mySplit[2].split('par ')[1].split(' ')[0];
+
+                var newItem = {
+                    id: id,
+                    content: contentPost,
+                    date: datePost,
+                    author: author,
+                };
+
+                vdmPosts.push(newItem);
+            });
+            console.log(vdmPosts);
+        } else {
+            console.log("Oops, I can't access " + url);
+        }
+    });
+}
